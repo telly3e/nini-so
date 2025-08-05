a448e8f90794b32747421ddd623daa9b1a1b1331
“telly3e” <telly3e@outlook.com>
2025-08-05 17:24:36 +0800

    change default search engine to baidu

--- js/local-storage.js
+++
@@ -1,6 +0,0 @@
-const userSettings = {
-    prefer_engine: 'baidu'
-};
-localStorage.setItem('settings', JSON.stringify(userSettings));
-
-let settings = JSON.parse(localStorage.getItem('settings'));
